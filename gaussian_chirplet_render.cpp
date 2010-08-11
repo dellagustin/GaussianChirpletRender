5c8737ec49abd88fadecae8369e23ca383b6c8cf
Guilherme Dellagustin <dellagustin@gmail.com>
2010-08-10 22:26:34 -0300

    Added rendering loops, and some comments.

--- gaussian_chirplet_render.cpp
+++ gaussian_chirplet_render.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <memory.h>
 
 using namespace std;
 
@@ -13,9 +14,11 @@ inline double square(double fValue)
 	return fValue*fValue;
 }
 
+//! \brief This class represents a gaussian chirplet
 class CGaussianChirplet
 {
 public:
+	//! \brief Contructor
 	CGaussianChirplet();
 
 	//! Calculate the value of the chirplet at time fTime
@@ -70,5 +73,51 @@ double CGaussianChirplet::value2d(double fTime, double fFrequency) const
 int main()
 {
     // cout << "Hello world!" << endl;
+    unsigned int i, j, k;
+    unsigned int nTimeSamples = 800;
+    unsigned int nFrequencySamples = 480;
+    // unsigned int nSamplesShift = 1;
+    double fTimeMin = 0.0;
+    double fTimeMax = 8.0;
+	double fFreqMin = 2.0;
+    double fFreqMax = 64.0;
+
+	CGaussianChirpletVector gcvector;
+
+	// TODO: here you need to read the atoms and fill gcvector
+
+    double *pfBitmap = new double[nTimeSamples*nFrequencySamples];
+
+    // reset the bitmap values
+    memset(pfBitmap, 0, nTimeSamples*nFrequencySamples*sizeof(double));
+
+    // steps in time
+    double fTimeStep = (fTimeMax - fTimeMin)/((double)nTimeSamples);
+
+    // steps in frequency
+    double fFreqStep = (fTimeMax - fTimeMin)/((double)nTimeFrequency);
+
+	// gaussian chirplet loop
+    for(k = 0; k < gcvector.size(); k++)
+	{
+		// time loop
+		for(i = 0; i < nTimeSamples; i++)
+		{
+			// frequency loop
+			for(j = 0; j < nFrequencySamples; j++)
+			{
+				double fTime = ((double)i)*fTimeStep;
+				double fFreq = ((double)j)*fFreqStep;
+
+				// fTime =
+				// fFreq =
+
+				pfBitmap[j*nFrequencySamples + i] = gcvector[i].value2d(fTime, fFreq);
+			}
+		}
+	}
+
+	// TODO: here you need to write the bitmap
+
     return 0;
 }
